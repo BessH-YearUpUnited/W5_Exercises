5ca44f41a758494906e95bfc90b16108666ba021
elizabeas <bess@rcc.team>
2024-10-22 14:05:31 -0500

    examples from 10/22 class

---
+++ lesson_10_22.py
@@ -0,0 +1,74 @@
+# fruits = 16
+# vegetables = 12
+# herbs = '11'
+
+# produce = fruits + vegetables # 16 + 12
+
+# produce += int(herbs)
+
+# print("Fruits: " + str(fruits))
+
+# print(produce) # 28
+
+# fruits = (fruits - 10) + (fruits + 15) # fruits = 6 + 31 = 37
+
+# print(fruits)
+
+# produce = fruits + vegetables
+
+# print(produce)
+
+# # def inventory(shipment):
+# #     storeroom = ''
+# #     for i in shipment:
+# #         storeroom + i
+# #     return storeroom
+
+# print(i)
+
+# fruits = 16
+# vegetables = 13
+# meals = 3
+
+# today_sides = vegetables / meals
+# print(today_sides)
+
+# sides2 = vegetables // meals
+# print(sides2)
+
+# veg_left = vegetables % meals
+# print(veg_left)
+
+
+## Home Improvement Calculator
+
+# Define known values
+
+starting_inches = 67
+
+# Calculate the unknown
+in_feet = starting_inches // 12
+
+rem_in = starting_inches % 12
+
+# Display the results
+
+print(in_feet, 'feet', rem_in, 'inches')
+
+
+
+# blocks = 7 * 25
+
+# print(blocks)
+
+# value = 3 + 10 * 2
+# value2 = (3 + 10) ** 2
+
+# print(value)
+# print(value2)
+
+
+num1 = '11'
+num2 = '10'
+print(int(num1) + int(num2))
+
